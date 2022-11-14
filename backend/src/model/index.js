ec440998e6c80ca8cc35e44927a4951854933fd3
Prajwol <prajwolneupane68@gmail.com>
2022-11-14 14:55:17 +0545

    Backend remove

--- backend/src/model/index.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-export default mongoose.connect(`mongodb+srv://${process.env.DB_OWNER}:${encodeURIComponent(process.env.DB_PASSWORD)}@cluster0.lecpeul.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`).then(() => {
-    console.log(`Database connected`);
-}).catch((e) => {
-    console.log(e.message);
-})
